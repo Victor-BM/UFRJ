09f9358bb98322711d15b6c5ec45323592d56c7c
Víctor BM <bernardes.morais2180@gmail.com>
2024-06-05 21:56:51 -0300

    Delete Computação I/Atividades/Laboratório 9/Lab9MT - Víctor BM.py

--- Computação I/Atividades/Laboratório 9/Lab9MT - Víctor BM.py
+++
@@ -1,9 +0,0 @@
-#LabMT 9 - Víctor BM
-#Q1
-def eh_quadrada (matriz):
-    '''Verifica se a matriz é quadrada
-    list -> bool'''
-    if len(matriz) == len(matriz[0]):
-        return True
-    else:
-        return False
