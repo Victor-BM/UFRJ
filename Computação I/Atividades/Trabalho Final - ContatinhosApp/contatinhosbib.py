f69c8473368b6c29de384486e531612ee31e3266
Víctor BM <bernardes.morais2180@gmail.com>
2024-06-26 21:39:07 -0300

    Update contatinhosbib.py

--- Computação I/Atividades/Trabalho Final - ContatinhosApp/contatinhosbib.py
+++ Computação I/Atividades/Trabalho Final - ContatinhosApp/contatinhosbib.py
@@ -1,38 +1,32 @@
-#Trabalho Final - ContatinhosApp - Víctor BM
+#Trabalho Final - ContatinhosApp - Parte 1 - Víctor BM
 #contatos = []#colocar isso no arquivo de interfacd
 def criar_contato (nome, telefone = '', email = '', instagram = ''):
     '''Função que permite criar contatos com apenas o nome obrigatório
     str, str, str, str -> list'''
+    contato = []
     telefones = [telefone,]
     contato1 = [nome, telefones, email, instagram]
-    list.append(contatos, contato1)
-    return contatos
+    list.append(contato, contato1)
+    return contato
 
 def atualizar_contato (contato, indice, nova_inf):
     '''Função que permite atualizar o contato adicionando uma nova informação pelo indice
     0 -> nome; 1 -> telefones; 2 -> email, 3 -> instagram
-    list, str, int, str -> bool'''
+    list, int, str -> list'''
     if indice == 0 or indice == 2 or indice == 3:
-        list.remove(contatos, contato)
         contato[indice] = nova_inf
-        list.append(contatos, contato)
-        return True
-    elif indice == 1 and (nova_inf != contato[1][0]):
-        list.remove(contatos, contato)
+    if indice == 1 and (nova_inf != contato[1]):
+        contato[1] = [contato[1]]
         list.append(contato[1], nova_inf)
-        list.append(contatos, contato)
-        return True
-    else:
-        return False 
+    contato = contato[::]
+    return contato
     
 def excluir_telefone (contato, telefone):
     '''Função que permite excluir o telefone do contato
-    list, string -> bool'''
+    list, string -> list'''
     if telefone in contato[1]:
         list.remove(contato[1], telefone)
-        return True
-    else:
-        return False
+    return contato
 
 def buscar_contato (contatos, nome):
     '''Função que busca um contato em uma lista de contatos
@@ -40,20 +34,24 @@ def buscar_contato (contatos, nome):
     i = 0
     resposta = []
     nome = nome.lower()
-    novo_contato = []
     while i<(len(contatos)):
         nome_contato = contatos[i][0]
         nome_contato = nome_contato.lower()
         if nome in nome_contato:
-            resposta.append(contatos[i])
+            resposta.append([i, contatos[i]])
         i += 1
     return resposta
 
-def quem_ligou(ligacao):
+def quem_ligou(contatos, ligacao):
     '''Função que retorna os dados de quem ligou
-    str -> list'''
-    for element in contatos:
-        if ligacao in element[1]:
-            return element
+    list, str -> list'''
+    for i in range(len(contatos)):
+        if type(contatos[i][1]) is list:
+            for j in range(len(contatos[i][1])):
+                if ligacao in contatos[i][1][j]:
+                    return contatos[i]
+        if ligacao in contatos[i][1]:
+            return contatos[i]
     else:
         return []
+#todas funções testadas e funcionais até aq
