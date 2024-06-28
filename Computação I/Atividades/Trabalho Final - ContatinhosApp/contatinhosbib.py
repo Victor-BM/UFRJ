32d644acbb6d08cc620f015a80fc6aa5fe431edd
Víctor BM <bernardes.morais2180@gmail.com>
2024-06-27 23:08:03 -0300

    Update contatinhosbib.py

--- Computação I/Atividades/Trabalho Final - ContatinhosApp/contatinhosbib.py
+++ Computação I/Atividades/Trabalho Final - ContatinhosApp/contatinhosbib.py
@@ -5,27 +5,22 @@ def criar_contato (nome, telefone = '', email = '', instagram = ''):
     str, str, str, str -> list'''
     telefones = [telefone,]
     contato1 = [nome, telefones, email, instagram]
-    list.append(contatos_geral, contato1)
-    return contatos_geral
+    return contato1
 
 def atualizar_contato (contato, indice, nova_inf):
     '''Função que permite atualizar o contato adicionando uma nova informação pelo indice
     0 -> nome; 1 -> telefones; 2 -> email, 3 -> instagram
     list, int, str -> bool'''
     ind = list.index(contatos_geral, contato)
-    if indice == 0 or indice == 2 or indice == 3:
+    if (indice == 0 or indice == 2 or indice == 3) and (nova_inf != ''):
         contato[indice] = nova_inf
         list.insert(contatos_geral, ind, contato)
         del contatos_geral[ind+1]
         return True
-    elif indice == 1 and (nova_inf != contato[1]) and not(type(contato[1]) is list):
-        contato[1] = [contato[1]]
+    elif indice == 1 and  (nova_inf not in contato[1]) and (nova_inf != ''):
         list.append(contato[1], nova_inf)
-        list.insert(contatos_geral, ind, contato)
-        del contatos_geral[ind+1]
-        return True
-    elif indice == 1 and (type(contato[1]) is list) and  (nova_inf not in contato[1]):
-        list.append(contato[1], nova_inf)
+        if '' in contato[1]:
+            del contato[1][0]
         list.insert(contatos_geral, ind, contato)
         del contatos_geral[ind+1]
         return True
@@ -80,33 +75,16 @@ def aglutinador (contatos, indice1, indice2):
     '''Função que, dado dois contatos, algutina eles. No entanto, mantém as informações
     do primeiro e adiciona caso haja algo novo do segundo
     list, int, int -> none'''
-    if (0 <= indice1 <= (len(contatos_geral) - 1)) and (0 <= indice1 <= (len(contatos_geral) - 1)) and (indice1 != indice2):
-        if (type(contatos_geral[indice1][1]) is list) and (type(contatos_geral[indice2][1]) is list):
-            for i in range(len(contatos_geral[indice2][1])):
-                if contatos_geral[indice2][1][i] not in contatos_geral[indice1][1]:
-                    list.append(contatos_geral[indice1][1], contatos_geral[indice2][1][i])
-        elif (type(contatos_geral[indice1][1]) is list) and (contatos_geral[indice2][1] not in contatos_geral[indice1][1]):
-            list.append(contatos_geral[indice1][1], contatos_geral[indice2][1])
-        elif (type(contatos_geral[indice2][1]) is list):
-            contatos_geral[indice1][1] = [contatos_geral[indice1][1]]
-            if contatos_geral[indice1][1][0] not in contatos_geral[indice2][1]:
-                list.extend(contatos_geral[indice1][1], contatos_geral[indice2][1])
-            else:
-                contatos_geral[indice1][1] = contatos_geral[indice2][1]
-        elif not (type(contatos_geral[indice1][1]) is list) and not (type(contatos_geral[indice2][1]) is list):
-            contatos_geral[indice1][1] = [contatos_geral[indice1][1]]
-            if contatos_geral[indice1][1][0] != contatos_geral[indice2][1]:
-                list.append(contatos_geral[indice1][1], contatos_geral[indice2][1])
-        if contatos_geral[indice1][2] != contatos_geral[indice2][2]:
-            if contatos_geral[indice2][2] != '':
-                contatos_geral[indice1][2] = [contatos_geral[indice1][2]]
-                list.append(contatos_geral[indice1][2], contatos_geral[indice2][2])
-            if contatos_geral[indice1][2][0] == '':
-                del contatos_geral[indice1][2][0]
-        if (contatos_geral[indice1][3] != contatos_geral[indice2][3]):
-            if contatos_geral[indice2][3] != '':
-                contatos_geral[indice1][3] = [contatos_geral[indice1][3]]
-                list.append(contatos_geral[indice1][3], contatos_geral[indice2][3])
-            if contatos_geral[indice1][3][0] == '':
-                del contatos_geral[indice1][3][0]
+    if (0 <= indice1 <= (len(contatos_geral) - 1)) and (0 <= indice2 <= (len(contatos_geral) - 1)) and (indice1 != indice2):
+        if contatos_geral[indice1][1][0] == '':
+            del contatos_geral[indice1][1][0]
+        for i in range(len(contatos_geral[indice2][1])):
+            if (contatos_geral[indice2][1][i] not in contatos_geral[indice1][1]):
+                list.append(contatos_geral[indice1][1], contatos_geral[indice2][1][i])
+        for j in range(2, 4):
+            if (contatos_geral[indice1][j] != contatos_geral[indice2][j]) and (contatos_geral[indice2][j] != ''):
+                contatos_geral[indice1][j] = [contatos_geral[indice1][j]]
+                list.append(contatos_geral[indice1][j], contatos_geral[indice2][j])
+                if contatos_geral[indice1][j][0] == '':
+                    del contatos_geral[indice1][j][0]
         del contatos_geral[indice2]
