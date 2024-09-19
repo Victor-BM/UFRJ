2c7f5a6c38d712e74278c8ad8143f26db1602e81
Víctor BM <bernardes.morais2180@gmail.com>
2024-09-19 03:45:58 -0300

    Update docs.py

---
+++ Computação II/Trabalho do Jogo - Herança/docs.py
@@ -0,0 +1,28 @@
+introdução = """Olá, Engenheiro do Fim do Mundo!
+
+Parabéns, você sobreviveu ao apocalipse nuclear. O seu maior pesadelo como Engenheiro Nuclear foi realizado!
+
+O melhor de tudo é que você foi o responsável por apertar aquele botão vermelho que gerou todo esse caos!
+
+Agora, você tem a chance única de enfrentar criaturas mutantes, passar por chuvas ácidas e algumas surpresinhas a mais.
+
+Sua missão é simples: sobreviver. Ok, talvez não seja tão simples, mas, por enquanto, você é nossa única esperança (estamos lascados).
+
+Então, você está pronto para, pelo menos, tentar não piorar as coisas?
+
+Sua energia, vida e sanidade estão completos, e você tem 20 pontos para distribuir entre:
+
+- Fisicalidade: Afeta sua força física;
+
+- Racionalidade: Afeta suas capacidade de resolver problemas;
+
+- Emocionalidade: Afeta sua estabilidade emocional.
+
+Como você quer distribuir esses pontos?"""
+
+texto_saida_bunker = """Você sai do bunker e percebe que o mundo está pior do que nas suas piores previsões de desastre nuclear. O mundo está silencioso e afetado pela radioatividade.
+Talvez voltar para o bunker não seja uma ideia tão ruim assim.. Quanto tempo será que a comida ainda vai durar?"""
+
+texto_evento_1 = """Talvez seja coragem, talvez seja desespero, mas você saiu do bunker e vai explorar a região.
+
+Você encontra um supermercado destruído e fica na dúvida se deve explorar o que ainda restou dentro dele ou continuar a sua jornada."""
